9066e70615b9aabae426b085f44bf0b89fef5b05
eloverflow <jfortin-moreau@mirageflow.com>
2016-08-17 11:27:12 -0400

    order gulp

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,7 @@ gulp.registry(hub);
 gulp.task('build', gulp.series('partials', gulp.parallel('other', 'webpack:dist')));
 gulp.task('test', gulp.series('karma:single-run'));
 gulp.task('test:auto', gulp.series('karma:auto-run'));
-gulp.task('serve', gulp.series('webServer', 'watch', 'browsersync', sassCompile));
+gulp.task('serve', gulp.series('watch', 'browsersync', sassCompile, 'webServer'));
 //gulp.task('serve', gulp.series('webpack:watch', 'watch', 'browsersync', sassCompile));
 gulp.task('serve:api', gulp.series('api:watch'));
 gulp.task('serve:dist', gulp.series('default', 'browsersync:dist'));
