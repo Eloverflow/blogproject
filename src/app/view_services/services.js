8d51840c79764d741f41a834058b2a1ab311f272
eloverflow <jfortin-moreau@mirageflow.com>
2016-10-14 10:52:07 -0400

    service animation compatible for phone, edge, ie, and other browser

--- src/app/view_services/services.js
+++ src/app/view_services/services.js
@@ -3,10 +3,12 @@
 angular.module('starter.controllers')
 
 .controller('PageServicesCtrl', function($scope, getReq, delReq, $location, API_ENDPOINT, postReq) {
+    var scale = 4;
     var mouseX = 0, mouseY = 0,
 
         windowHalfX = window.innerWidth / 2,
         windowHalfY = window.innerHeight / 2,
+
 
         SEPARATION = 200,
         AMOUNTX = 1,
@@ -30,86 +32,106 @@ angular.module('starter.controllers')
 
         function init() {
 
+            if (screen.width > 480) {
 
-            if(screen.width > 480){
-                /*
-                 *   Define variables
-                 */
-                var container, separation = 1000, amountX = 50, amountY = 50, color = 0x333333,
-                    particles, particle;
+                if (/MSIE 10/i.test(navigator.userAgent) ||
+                    /MSIE 9/i.test(navigator.userAgent) ||
+                    /rv:11.0/i.test(navigator.userAgent) ||
+                    /Edge\/\d./i.test(navigator.userAgent)) {
+                    // This is internet explorer 10
+                    nbrLine = 200;
+                }else if(/Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor)){
+                    nbrLine = 600;
+                }else {
+                    nbrLine = 400;
+                }
 
-                container = document.getElementById("canvas");
+            }
+            else {
+                nbrLine = 120;
+                scale = 10;
+            }
+
+            /*
+             *   Define variables
+             */
+            var container, separation = 1000, amountX = 50, amountY = 50, color = 0x333333,
+                particles, particle;
+
+            container = document.getElementById("canvas");
 
 
-                camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 20000 );
-                camera.position.z = 100;
+            camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 20000);
+            camera.position.z = 100;
 
-                scene = new THREE.Scene();
+            scene = new THREE.Scene();
 
-                renderer = new THREE.CanvasRenderer({ alpha: true });
-                renderer.setPixelRatio( window.devicePixelRatio );
-                renderer.setClearColor( 0x000000, 0 );   // canvas background color
-                renderer.setSize( window.innerWidth, window.innerHeight );
-                container.appendChild( renderer.domElement );
+            renderer = new THREE.CanvasRenderer({alpha: true});
+            renderer.setPixelRatio(window.devicePixelRatio);
+            renderer.setClearColor(0x000000, 0);   // canvas background color
+            renderer.setSize(window.innerWidth, window.innerHeight);
+            container.appendChild(renderer.domElement);
 
 
+            var PI2 = Math.PI * 2;
+            var material = new THREE.SpriteCanvasMaterial({
 
-                var PI2 = Math.PI * 2;
-                var material = new THREE.SpriteCanvasMaterial( {
+                color: "#286090",
+                opacity: 1,
+                program: function (context) {
 
-                    color: "#286090",
-                    opacity: 1,
-                    program: function ( context ) {
-
-                        context.beginPath();
-                        context.arc( 0, 0, 0.3, 0, PI2, true );
-                        context.fill();
-
-                    }
-
-                } );
-
-                var geometry = new THREE.Geometry();
-
-                /*
-                 *   Number of particles
-                 */
-                for ( var i = 0; i < 600; i ++ ) {
-
-                    particle = new THREE.Sprite( material );
-                    particle.position.x = Math.random() * 2 - 1;
-                    particle.position.y = Math.random() * 2 - 1;
-                    particle.position.z = Math.random() * 2 - 1;
-                    particle.position.normalize();
-                    particle.position.multiplyScalar( Math.random() * 10 + 100 );
-                    particle.scale.x = particle.scale.y = 4;
-
-                    scene.add( particle );
-
-                    geometry.vertices.push( particle.position );
+                    context.beginPath();
+                    context.arc(0, 0, 0.3, 0, PI2, true);
+                    context.fill();
 
                 }
 
-                /*
-                 *   Lines
-                 */
+            });
 
-                var line = new THREE.Line( geometry, new THREE.LineBasicMaterial( { color: color, opacity: 0.3 } ) );
-                scene.add( line );
+            var geometry = new THREE.Geometry();
 
-                document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-                document.addEventListener( 'touchstart', onDocumentTouchStart, false );
-                document.addEventListener( 'touchmove', onDocumentTouchMove, false );
+            /*
+             *   Number of particles
+             */
+            for (var i = 0; i < nbrLine; i++) {
 
-                //
+                particle = new THREE.Sprite(material);
+                particle.position.x = Math.random() * 2 - 1;
+                particle.position.y = Math.random() * 2 - 1;
+                particle.position.z = Math.random() * 2 - 1;
+                particle.position.normalize();
+                particle.position.multiplyScalar(Math.random() * 10 + 100);
+                particle.scale.x = particle.scale.y = scale;
 
-                window.addEventListener( 'resize', onWindowResize, false );
-            }else {
-                $('#canvas').remove();
+                scene.add(particle);
+
+                geometry.vertices.push(particle.position);
+
             }
 
+            /*
+             *   Lines
+             */
+
+            var line = new THREE.Line(geometry, new THREE.LineBasicMaterial({color: color, opacity: 0.3}));
+            scene.add(line);
+
+            if (screen.width > 480)
+            document.addEventListener('mousemove', onDocumentMouseMove, false);
+
+            document.addEventListener('touchstart', onDocumentTouchStart, false);
+            document.addEventListener('touchmove', onDocumentTouchMove, false);
+            window.onscroll = function() {onDocumentScroll()};
+            //
+
+            window.addEventListener('resize', onWindowResize, false);
+            /*}else {
+             $('#canvas').remove();
+             }*/
 
         }
+
+
 
         function onWindowResize() {
 
@@ -132,14 +154,21 @@ angular.module('starter.controllers')
 
         }
 
+        function onDocumentScroll(event) {
+
+            //mouseX = (event.clientX - windowHalfX) * 0.05 * 3;
+            mouseY = (document.body.scrollTop - windowHalfY) * 0.2 * 2+120;
+
+        }
+
         function onDocumentTouchStart( event ) {
 
             if ( event.touches.length > 1 ) {
 
-                event.preventDefault();
+                //event.preventDefault();
 
-                mouseX = (event.touches[ 0 ].pageX - windowHalfX) * 0.7;
-                mouseY = (event.touches[ 0 ].pageY - windowHalfY) * 0.7;
+                mouseX = (event.touches[ 0 ].pageX - windowHalfX) * 0.4;
+                //mouseY = (event.touches[ 0 ].pageY - windowHalfY) * 0.4;
 
             }
 
@@ -149,10 +178,10 @@ angular.module('starter.controllers')
 
             if ( event.touches.length == 1 ) {
 
-                event.preventDefault();
+                //event.preventDefault();
 
                 mouseX = event.touches[ 0 ].pageX - windowHalfX;
-                mouseY = event.touches[ 0 ].pageY - windowHalfY;
+                //mouseY = event.touches[ 0 ].pageY - windowHalfY;
 
             }
 
