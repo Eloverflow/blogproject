83e33b3ce4b9bd78490a9e7eb0abf452e7ce95da
eloverflow <jfortin-moreau@outlook.com>
2017-01-05 13:40:13 -0500

    call to action modal begin

--- src/app/view_home/home.js
+++ src/app/view_home/home.js
@@ -1,7 +1,16 @@
 'use strict';
 angular.module('starter.controllers')
-.controller('HomeCtrl', function($scope, DEBUG, API_ENDPOINT, $translate) {
+.controller('HomeCtrl', function($scope, $rootScope, DEBUG, API_ENDPOINT, $translate) {
 
+
+    if(DEBUG.isEnabled){
+        if(!$rootScope.callToActionModalProc){
+            setTimeout(function () {
+                $('#callToActionModal').modal('show');
+                $rootScope.callToActionModalProc = true;
+            },1000)
+        }
+    }
 
 });
 
